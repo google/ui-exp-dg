828dc832d02c4f680017cf2186873b622465eb6c
Jim Sproch <jsproch@google.com>
2020-06-16 21:06:44 -0700

    Add simple iOS app with libskia bindings for both simulator and arm64

---
+++ HelloWOrld/HelloWOrld/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HelloWOrld
+//
+//  Created by Jim Sproch on 6/9/20.
+//  Copyright © 2020 Jim Sproch. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World 3!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HelloWOrld/README.md
@@ -0,0 +1,4 @@
+
+To learn more about this repository:
+[HelloSkia status doc](https://docs.google.com/document/d/1dkMMApQ1QSXwSYcgfb65yF-jUlJKzHZ4WUDnak2C16U/edit#)
+
